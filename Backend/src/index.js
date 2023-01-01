5f7bd829d5037305337d53cc2711ad2a58ab7da7
SatyamRandawa <satyamrandwa141@gmail.com>
2023-01-01 13:28:40 +0530

    Ludo first push

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -40,8 +40,8 @@ app.use('/', userRoutes);
 
 
 
-app.listen(process.env.Port || 3400, function () {
-    console.log('App running on port ' + (process.env.PORT || 3400))
+app.listen(process.env.Port || 8080, function () {
+    console.log('App running on port ' + (process.env.PORT || 8080))
 });
 
 
